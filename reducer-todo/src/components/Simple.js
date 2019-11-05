bfce4676f7312045ef4235cbd3e311aa86d9dae9
Darrin Lowery <lambda@monsto.com>
2019-11-05 17:31:29 -0600

    progressing

--- reducer-todo/src/components/Simple.js
+++ reducer-todo/src/components/Simple.js
@@ -2,30 +2,38 @@ import React, { useReducer, useState } from "react";
 import { appReducer, initState } from "../reducers/reductor";
 
 export const Simple = () => {
-	const [todo, setTodo] = useState([]);
+	const [todo, setTodo] = useState(initState);
 	const [state, dispatch] = useReducer(appReducer, initState);
 
 	const handleChange = event => {
-		console.log(todo);
+		// console.log(todo);
 		setTodo(event.target.value);
 	};
 
 	const handleSubmit = event => {
 		event.preventDefault();
-		setTodo(event.target.value);
-		// dispatch
+		dispatch({
+			type: "ADD_ITEM",
+			payload: {
+				item: event.target.value,
+				completed: false,
+				id: 7
+			}
+		});
 	};
 
 	return (
 		<div>
 			<form onSubmit={handleSubmit}>
 				<input onChange={handleChange} value={todo} />
-				{todo.map(item => (
+			</form>
+			<div>
+				{state.map(item => (
 					<div key={item.id}>
 						<p>{item.item}</p>
 					</div>
 				))}
-			</form>
+			</div>
 		</div>
 	);
 };
--- reducer-todo/src/reducers/reductor.js
+++ reducer-todo/src/reducers/reductor.js
@@ -1,17 +1,29 @@
 import React, { useReducer, useState } from "react";
 
-export const initState = {
-	item: "Learn about reducers",
-	completed: false,
-	id: 3892987589
-};
+export const initState = [
+	{
+		item: "Learn about reducers",
+		completed: false,
+		id: 1
+	},
+	{
+		item: "Second",
+		completed: false,
+		id: 2
+	}
+];
 
 export const appReducer = (state, action) => {
+	console.log(state);
 	switch (action.type) {
-		case "TEMP":
-			return { ...state };
+		case "ADD_ITEM":
+			return {
+				...state,
+				item: state.item,
+				completed: state.completed,
+				id: state.id
+			 };
 		default:
 			throw new Error("Problem: No action matched.");
 	}
 };
-
